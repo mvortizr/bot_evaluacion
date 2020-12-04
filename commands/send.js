e4e4c28d69ed79d1e0b25d41e59878a620c0cc01
María V. Ortiz <mariatua2@gmail.com>
2020-12-04 17:07:42 -0400

    Lista parte de attachments

--- commands/send.js
+++ commands/send.js
@@ -1,13 +1,20 @@
 const {MessageAttachment} = require('discord.js');
 const { performance } = require('perf_hooks');
+const  fs = require("fs"); //Load the filesystem module
+
 
 module.exports = {
     name:'send',
     description: 'send attachment to channel',
     async execute(message,args){
        
+        console.log('args',args);
         try {
-            const attachment = await new MessageAttachment('https://www.net-aware.org.uk/siteassets/images-and-icons/application-icons/app-icons-discord.png?width=270');
+            let attachment = await new MessageAttachment(`./filesToUpload/${args[0]}`);
+            //obtener size
+            const stats = fs.statSync(`./filesToUpload/${args[0]}`)
+            attachment.size = stats.size
+            console.log('attachment',attachment);
             console.log('attachment cargado')
             const t0 = performance.now();
             await message.channel.send(attachment);
@@ -16,7 +23,7 @@ module.exports = {
             console.log(`Time taken ${miliseconds} miliseconds, ${miliseconds/1000} seconds`)
             console.log(`Attachment size ${attachment.size} bytes`)
             await message.channel.send(`Time taken ${miliseconds} miliseconds, ${miliseconds/1000} seconds`);
-            await message.channel.send(`Attachment size ${attachment.size} bytes`);
+            await message.channel.send(`Attachment size ${attachment.size} bytes, ${attachment.size/1000}Kb, ${attachment.size/1000000}MB`);
         } catch (e) {
             console.log(`ERROR ${e}`)
         }
